bd5ac24f120cb13b23406ec005d07ec2692239ec
unknown <workmail2159@gmail.com>
2024-05-24 18:42:57 -0600

    slightly less UV warping

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,22 @@
+use std::f32::consts::PI;
+
 use bevy::{
-    log::tracing_subscriber::fmt::format, math::{vec2, vec3}, prelude::*, render::{
-        camera::RenderTarget, mesh::{Indices, PlaneMeshBuilder}, render_asset::RenderAssetUsages, render_resource::{Extent3d, PrimitiveTopology, TextureDescriptor, TextureDimension, TextureFormat, TextureUsages}, settings::*, RenderPlugin
-    } 
+    ecs::bundle,
+    log::tracing_subscriber::fmt::format,
+    math::{vec2, vec3},
+    prelude::*,
+    render::{
+        camera::RenderTarget,
+        mesh::{Indices, PlaneMeshBuilder},
+        render_asset::RenderAssetUsages,
+        render_resource::{
+            Extent3d, PrimitiveTopology, TextureDescriptor, TextureDimension, TextureFormat,
+            TextureUsages,
+        },
+        settings::*,
+        RenderPlugin,
+    },
+    ui::node_bundles,
 };
 fn main() {
     App::new()
@@ -10,7 +25,6 @@ fn main() {
         //.add_systems(Update, rotate)
         .run();
 }
-
 
 pub struct MyRenderPlugin;
 /// This plugin is copied from https://github.com/bevyengine/bevy/issues/9975 to fix an issue that was throwing an obscene number of
@@ -38,7 +52,6 @@ impl Plugin for MyRenderPlugin {
     }
 }
 
-
 /// A marker component for our shapes so we can query them separately from the ground plane
 #[derive(Component)]
 struct Hex {
@@ -49,7 +62,6 @@ struct Hex {
 // HEX Const values
 pub const HEX_OUTER_RADIUS: f32 = 2.0;
 pub const HEX_INNER_RADIUS: f32 = HEX_OUTER_RADIUS * 0.866025404;
-
 
 fn setup(
     mut commands: Commands,
@@ -70,9 +82,7 @@ fn setup(
 
     let shape = meshes.add(create_hex_mesh());
 
-
-    
-    for z in -10..10{
+    for z in -10..10 {
         for x in -10..10 {
             let mut image = Image {
                 texture_descriptor: TextureDescriptor {
@@ -89,48 +99,73 @@ fn setup(
                 },
                 ..default()
             };
+            //image = uv_debug_texture();
             // fill image.data with zeroes
             image.resize(size);
-        
+
             let image_handle = images.add(image);
             let texture_camera = commands
-            .spawn(Camera2dBundle {
-                camera: Camera {
-                    // render before the "main pass" camera
-                    order: -1,
-                    target: RenderTarget::Image(image_handle.clone()),
+                .spawn(Camera2dBundle {
+                    camera: Camera {
+                        // render before the "main pass" camera
+                        order: -1,
+                        target: RenderTarget::Image(image_handle.clone()),
+                        clear_color: ClearColorConfig::Custom(Color::GOLD),
+                        ..default()
+                    },
                     ..default()
-                },
-                ..default()
-            })
-            .id();
-            
+                })
+                .id();
+
             let material_handle = materials.add(StandardMaterial {
                 base_color_texture: Some(image_handle),
                 reflectance: 0.02,
                 unlit: false,
+                alpha_mode:AlphaMode::Mask(0.5),
                 ..default()
             });
-            let position = vec3((x as f32 + z as f32 * 0.5 - (z /2) as f32) * (HEX_INNER_RADIUS * 2.0), 
-                                0.0,
-                                z as f32 * HEX_OUTER_RADIUS * 1.5);
+            let position = vec3(
+                (x as f32 + z as f32 * 0.5 - (z / 2) as f32) * (HEX_INNER_RADIUS * 2.0),
+                0.0,
+                z as f32 * HEX_OUTER_RADIUS * 1.5,
+            );
             let index = vec3(x as f32, (-x - z) as f32, z as f32);
             commands.spawn((
                 PbrBundle {
                     mesh: shape.clone(),
-                    material: material_handle.clone(),
+                    material: materials.add(StandardMaterial {
+                                    base_color_texture: Some(images.add(uv_debug_texture())),
+                                    ..default()
+                                }),
                     transform: Transform::from_xyz(position.x, position.y, position.z)
-                    .with_rotation(Quat::from_rotation_x(0.0)),
+                        .with_rotation(Quat::from_rotation_x(0.0)),
                     ..default()
                 },
-                Hex{
-                    position: {position},
-                    index: {index},
+                Hex {
+                    position: { position },
+                    index: { index },
                     color: Color::SILVER,
                 },
-                
             ));
-            commands.spawn((
+            // commands.spawn((
+            //     PbrBundle {
+            //         mesh: shape.clone(),
+            //         material: materials.add(StandardMaterial {
+            //             base_color_texture: Some(images.add(uv_debug_texture())),
+            //             ..default()
+            //         }),
+            //         transform: Transform::from_xyz(position.x, position.y, position.z)
+            //             .with_rotation(Quat::from_rotation_x(0.0)),
+            //         ..default()
+            //     },
+            //     Hex {
+            //         position: { position },
+            //         index: { index },
+            //         color: Color::SILVER,
+            //     },
+            // ));
+            commands
+            .spawn((
                 NodeBundle {
                     style: Style {
                         // Cover the whole image
@@ -141,14 +176,14 @@ fn setup(
                         align_items: AlignItems::Center,
                         ..default()
                     },
-                    background_color: Color::GOLD.into(),
+                    background_color: BackgroundColor(Color::Rgba { red: 0.0, green: 0.0, blue: 0.0, alpha: 0.0 }),
                     ..default()
                 },
-            TargetCamera(texture_camera),
+                TargetCamera(texture_camera),
             ))
             .with_children(|parent| {
                 parent.spawn(TextBundle::from_section(
-                    format!("Index\nX: {}\nY: {}\nZ: {}", index.x, index.y, index.z),
+                    "This is a cube",
                     TextStyle {
                         font_size: 40.0,
                         color: Color::BLACK,
@@ -156,6 +191,26 @@ fn setup(
                     },
                 ));
             });
+            
+            // commands.spawn(
+            //     PbrBundle {
+            //         mesh: meshes.add(Cuboid::new(HEX_INNER_RADIUS, 0.1, HEX_INNER_RADIUS)),
+            //         material:material_handle,
+            //         ..default()
+
+            //     }
+            // );
+            commands.spawn(
+                PbrBundle {
+                    mesh: meshes.add(Cuboid::new(HEX_INNER_RADIUS, 0.1, HEX_INNER_RADIUS)),
+                    material: materials.add(StandardMaterial {
+                                    base_color_texture: Some(images.add(uv_debug_texture())),
+                                    ..default()
+                                }),
+                    ..default()
+
+                }
+            );
         }
     }
     commands.spawn(PointLightBundle {
@@ -175,56 +230,56 @@ fn setup(
     });
 }
 
-        fn create_hex_mesh() -> Mesh{
-            Mesh::new(PrimitiveTopology::TriangleList, RenderAssetUsages::MAIN_WORLD | RenderAssetUsages::RENDER_WORLD)
-            .with_inserted_attribute(
-                Mesh::ATTRIBUTE_POSITION,
-                        //Mesh vertices
-                vec![[0.0,0.0,0.0],
-                            [0.0,0.0,HEX_OUTER_RADIUS],
-                            [HEX_INNER_RADIUS,0.0,0.5 * HEX_OUTER_RADIUS],
-                            [HEX_INNER_RADIUS,0.0,-0.5*HEX_OUTER_RADIUS],
-                            [0.0,0.0,-HEX_OUTER_RADIUS],
-                            [-HEX_INNER_RADIUS, 0.0, -0.5*HEX_OUTER_RADIUS],
-                            [-HEX_INNER_RADIUS, 0.0, 0.5*HEX_OUTER_RADIUS],
-                            ]
-            )
-            .with_inserted_attribute(
-                Mesh::ATTRIBUTE_UV_0,
-                vec![
-                    // Assigning the UV coords for the top side.
-                    [0.5,0.5], 
-                    [0.0, 0.5], 
-                    [0.3333 , 1.0], 
-                    [0.6666,1.0],
-                    [1.0,0.5],
-                    [0.6666,0.0], 
-                    [0.3333,0.0],
-                        ]
-                    
-            )
-            .with_inserted_attribute(
-                Mesh::ATTRIBUTE_NORMAL,
-                vec![
-                    // Normals for the top side (towards +y)
-                    [0.0, 1.0, 0.0],
-                    [0.0, 1.0, 0.0],
-                    [0.0, 1.0, 0.0],
-                    [0.0, 1.0, 0.0],
-                    [0.0, 1.0, 0.0],
-                    [0.0, 1.0, 0.0],
-                    [0.0, 1.0, 0.0],])
-            .with_inserted_indices(Indices::U32(vec![
-                0,1,2,
-                0,2,3,
-                0,3,4,
-                0,4,5,
-                0,5,6,
-                0,6,1,
-                ])) // triangles making up the top (+y) facing side.
-        }
-        
-
+fn create_hex_mesh() -> Mesh {
+    let opposite_leg:f32 = ((PI / 6.0).sin() * HEX_INNER_RADIUS) / HEX_OUTER_RADIUS;
+    print!("{}, {}, {}",opposite_leg, 0.5 - opposite_leg, 0.5 + opposite_leg);
+    Mesh::new(
+        PrimitiveTopology::TriangleList,
+        RenderAssetUsages::MAIN_WORLD | RenderAssetUsages::RENDER_WORLD,
+    )
+    .with_inserted_attribute(
+        Mesh::ATTRIBUTE_POSITION,
+        //Mesh vertices
+        vec![
+            [0.0, 0.0, 0.0],
+            [0.0, 0.0, HEX_OUTER_RADIUS],
+            [HEX_INNER_RADIUS, 0.0, 0.5 * HEX_OUTER_RADIUS],
+            [HEX_INNER_RADIUS, 0.0, -0.5 * HEX_OUTER_RADIUS],
+            [0.0, 0.0, -HEX_OUTER_RADIUS],
+            [-HEX_INNER_RADIUS, 0.0, -0.5 * HEX_OUTER_RADIUS],
+            [-HEX_INNER_RADIUS, 0.0, 0.5 * HEX_OUTER_RADIUS],
+        ],
+    )
+    .with_inserted_attribute(
+        Mesh::ATTRIBUTE_UV_0,
+        vec![
+            // Assigning the UV coords for the top side.
+            [0.5,0.5], 
+            [0.0, 0.5], 
+            [0.5-opposite_leg , 1.0], 
+            [0.5+opposite_leg,1.0],
+            [1.0,0.5],
+            [0.5+opposite_leg,0.0], 
+            [0.5-opposite_leg,0.0],
+                ]
+    )
+    .with_inserted_attribute(
+        Mesh::ATTRIBUTE_NORMAL,
+        vec![
+            // Normals for the top side (towards +y)
+            [0.0, 1.0, 0.0],
+            [0.0, 1.0, 0.0],
+            [0.0, 1.0, 0.0],
+            [0.0, 1.0, 0.0],
+            [0.0, 1.0, 0.0],
+            [0.0, 1.0, 0.0],
+            [0.0, 1.0, 0.0],
+        ],
+    )
+    .with_inserted_indices(Indices::U32(vec![
+        0, 1, 2, 0, 2, 3, 0, 3, 4, 0, 4, 5, 0, 5, 6, 0, 6, 1,
+    ])) // triangles making up the top (+y) facing side.
+}
 
 fn uv_debug_texture() -> Image {
     const TEXTURE_SIZE: usize = 8;
